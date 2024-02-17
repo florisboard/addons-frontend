25421392b5150d06ded3f779eee84682cf785df0
4H1R <ghostrockz3d@gmail.com>
2024-02-17 21:10:31 +0330

    wip

--- src/shared/forms/ReactSelect.tsx
+++ src/shared/forms/ReactSelect.tsx
@@ -12,6 +12,7 @@ export default function ReactSelect({ className, menuClassName, ...props }: Reac
       {...props}
       components={{ DropdownIndicator: () => null }}
       unstyled
+      menuPosition="fixed"
       classNames={{
         container: () => cn(className, 'font-sans'),
         control: ({ isFocused, hasValue }) =>
@@ -19,18 +20,18 @@ export default function ReactSelect({ className, menuClassName, ...props }: Reac
             'border-2 border-primary': isFocused,
             'h-auto': props.isMulti && hasValue,
           }),
-        placeholder: () => cn('text-gray-600'),
-        indicatorsContainer: () => cn('gap-1'),
-        clearIndicator: () => cn('btn btn-circle btn-ghost btn-sm p-1'),
-        dropdownIndicator: () => cn('btn btn-circle btn-ghost btn-sm p-1'),
-        noOptionsMessage: () => cn('py-4 font-display text-base'),
+        placeholder: () => 'text-gray-600',
+        indicatorsContainer: () => 'gap-1',
+        clearIndicator: () => 'btn btn-circle btn-ghost btn-sm p-1',
+        dropdownIndicator: () => 'btn btn-circle btn-ghost btn-sm p-1',
+        noOptionsMessage: () => 'py-4 font-display text-base',
         menu: () =>
           cn(
             'mt-1 min-h-[2rem] rounded-btn border border-base-300 bg-base-200 px-2 shadow',
             menuClassName,
           ),
-        multiValue: () => cn('m-1 rounded-btn bg-base-200 p-1 px-2 hover:bg-base-300'),
-        multiValueRemove: () => cn('btn btn-circle btn-ghost btn-xs ml-1'),
+        multiValue: () => 'm-1 rounded-btn bg-base-200 p-1 px-2 hover:bg-base-300',
+        multiValueRemove: () => 'btn btn-circle btn-ghost btn-xs ml-1',
         option: ({ isFocused, isSelected }) =>
           cn('my-2 rounded-btn px-2 py-3 text-base hover:cursor-pointer', {
             'bg-primary text-primary-content': isSelected,
