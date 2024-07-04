a97b82f1633cd555325f801f08e5bd2a03856567
4H1R <ghostrockz3d@gmail.com>
2024-07-04 21:27:26 +0330

    generate metadata for projects show page

--- src/app/projects/[slug]/layout.tsx
+++ src/app/projects/[slug]/layout.tsx
@@ -9,24 +9,24 @@ type Props = {
   params: { slug: string };
 };
 
-// export async function generateMetadata({ params }: Props): Promise<Metadata> {
-//   const id = extractIdFromSlug(params.slug);
-//   if (!id) notFound();
-//   const project = await getProjectServerCache(id);
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const id = extractIdFromSlug(params.slug);
+  if (!id) notFound();
+  const project = await getProjectServerCache(id);
 
-//   // the project is not published the owner can see it so we create a simple metadata
-//   if (!project) {
-//     return {
-//       title: 'Project',
-//       description: 'Project',
-//     };
-//   }
+  // the project is not published the owner can see it so we create a simple metadata
+  if (!project) {
+    return {
+      title: 'Project',
+      description: 'Project',
+    };
+  }
 
-//   return {
-//     title: project.title,
-//     description: project.short_description,
-//   };
-// }
+  return {
+    title: project.title,
+    description: project.short_description,
+  };
+}
 
 export default function Layout({ children }: THasChildren) {
   return children;
